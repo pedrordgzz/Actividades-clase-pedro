03e4834caa73b16dd5528fed6b00a8c9f5ad5715
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-11-26 08:41:16 +0100

    creacion base

---
+++ actividades_diapositivas/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Header } from './assets/componentes/header';
+import { Navbar
+
+ } from './assets/componentes/navbar';
+export default function App() {
+  return (
+    <div className="app-container">
+      <Header />
+      <Navbar />
+
+      <main className="app-actividad">
+         
+      </main>
+
+
+    </div>
+  )
+};
